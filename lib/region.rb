1989490ec4bc6c66a0486e80be52e90df5d1dd9c
JacksonReynolds <jwreynod@gmail.com>
2020-07-27 13:37:06 -0400

    added class methods to Region

--- lib/region.rb
+++ lib/region.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 class Region
 
@@ -8,7 +8,6 @@ class Region
 
     def initialize(name)
         @name = name
-        self.all << self if self.all.include?()
     end # initialize
 
     def self.all
@@ -16,11 +15,26 @@ class Region
     end # self.all
 
     def self.create(name)
-        new_region = self.new(name)
+        a = self.new(name)
+        self.all << a
     end # self.create
+
+    def self.find_by_name(name)
+        self.all.select {|a| a.name == name}
+    end # self.find_by_name
+
+    def self.find_or_create_by_name(name)
+        inst = self.find_by_name(name)
+        inst ? inst : self.create(name)
+    end # find_or_create_by_name
 
     def zones
         Zone.all.select {|zone| zone.region == self}
     end # zones
 
 end # Region
+
+tenn = Region.create('Tennessee')
+
+binding.pry
+
