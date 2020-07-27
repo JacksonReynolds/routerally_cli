29d9c8f1a250b6df2e9ad7d8edc5de657e59e56c
JacksonReynolds <jwreynod@gmail.com>
2020-07-27 13:12:19 -0400

    added class files, began building region class

--- lib/region.rb
+++ lib/region.rb
@@ -8,17 +8,19 @@ class Region
 
     def initialize(name)
         @name = name
-        self.all << self
+        self.all << self if self.all.include?()
     end # initialize
 
     def self.all
         @@all
     end # self.all
 
+    def self.create(name)
+        new_region = self.new(name)
+    end # self.create
+
     def zones
         Zone.all.select {|zone| zone.region == self}
     end # zones
-
-
 
 end # Region
