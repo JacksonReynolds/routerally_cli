cbb0d74486a3fac5941a818049962e637771e511
Jackson <jwreynold@gmail.com>
2021-04-05 12:12:38 -0400

    added dotenv

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'pry'
 gem 'require_all'
 gem 'httparty'#, '~> 0.18.1'
+gem 'dotenv' 
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 
+require 'dotenv/load'
+
 require_all 'lib'
 require 'httparty'
 require 'pry'
